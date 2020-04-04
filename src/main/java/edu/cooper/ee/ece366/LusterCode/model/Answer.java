00e8ab7e6e3e5b1dc5c0d2e71190d3506868d3f1
jasonkurian <jkurian49@gmail.com>
2020-04-03 20:39:18 -0400

    create answers

--- src/main/java/edu/cooper/ee/ece366/LusterCode/model/Answer.java
+++ src/main/java/edu/cooper/ee/ece366/LusterCode/model/Answer.java
@@ -12,21 +12,21 @@ public class Answer {
     private final Long askPostID;
     private final String answerType; // with code or post reply
     private final String content; // either a file path to code or a reply text
-    private final Timestamp timestamp;
+    //private final Timestamp timestamp;
 
     public Answer(String username, Long askPostID, String answerType, String content){
         this.username = username;
         this.askPostID = askPostID;
         this.answerType = answerType;
         this.content = content;
-        timestamp = new Timestamp(System.currentTimeMillis());
+        //timestamp = new Timestamp(System.currentTimeMillis());
     }
 
     public String getUsername() { return username; }
     public Long getAskPostID() { return askPostID; }
     public String getAnswerType() { return answerType; }
     public String getContent() { return content; }
-    public Timestamp getTime() { return timestamp; }
+    //public Timestamp getTime() { return timestamp; }
 
     static final AtomicLong NEXT_ID = new AtomicLong(0);
     final long answerID = NEXT_ID.getAndIncrement();
