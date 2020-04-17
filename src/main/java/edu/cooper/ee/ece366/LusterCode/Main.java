a25a34859bf319d61dccb091f16b59b57629be9a
Zachary Friedman <friedmanjzachary.com>
2020-04-16 23:52:28 -0400

    Broke up User functionality

--- src/main/java/edu/cooper/ee/ece366/LusterCode/Main.java
+++ src/main/java/edu/cooper/ee/ece366/LusterCode/Main.java
@@ -10,22 +10,22 @@ import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
-        Gson gson = new Gson();
-        String url = "jdbc:mysql://localhost:3306/LusterCode?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=EST";
-        String username = "main";
-        String password = "Lusterman";
-        Jdbi jdbi = Jdbi.create(url, username, password);
-        AnswerStoreJdbi answerStore = new AnswerStoreJdbi(jdbi);
-        answerStore.populateDb();
-        AnswerService answerService = new AnswerService(answerStore);
-        AnswerHandler answerHandler = new AnswerHandler(answerService, gson);
-        JsonTransformer jsonTransformer = new JsonTransformer();
+//        Gson gson = new Gson();
+//        String url = "jdbc:mysql://localhost:3306/LusterCode?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=EST";
+//        String username = "main";
+//        String password = "Lusterman";
+//        Jdbi jdbi = Jdbi.create(url, username, password);
+//        AnswerStoreJdbi answerStore = new AnswerStoreJdbi(jdbi);
+//        answerStore.populateDb();
+//        AnswerService answerService = new AnswerService(answerStore);
+//        AnswerHandler answerHandler = new AnswerHandler(answerService, gson);
+//        JsonTransformer jsonTransformer = new JsonTransformer();
+//
+//        Spark.exception(Exception.class, (exception, request, response) -> {
+//            exception.printStackTrace();
+//        });
 
-        Spark.exception(Exception.class, (exception, request, response) -> {
-            exception.printStackTrace();
-        });
-
-        Spark.get("/ping", (req, res) -> "OK");
+        Spark.get("/ping", (req, res) -> "OK\n");
         // user routing
         // Spark.post("/user", (req, res) -> userHandler.createUser(req), jsonTransformer);
         // Spark.get("/user/:userID", (req, res) -> userHandler.getUser(req), jsonTransformer);
@@ -33,11 +33,11 @@ public class Main {
         // Spark.post("/post", (req, res) -> postHandler.createPost(req), jsonTransformer);
         // Spark.get("/post/:postID", (req, res) -> postHandler.getPost(req), jsonTransformer);
         // answer routing
-        Spark.post("/answer", answerHandler::createAnswer, gson::toJson);
-        Spark.get("/answer/:answerID", answerHandler::getAnswer, gson::toJson);
-        Spark.get("/answers/:askPostID", answerHandler::getAnswers, gson::toJson);
-        Spark.delete("/answer/:answerID", answerHandler::deleteAnswer, gson::toJson);
-
+//        Spark.post("/answer", answerHandler::createAnswer, gson::toJson);
+//        Spark.get("/answer/:answerID", answerHandler::getAnswer, gson::toJson);
+//        Spark.get("/answers/:askPostID", answerHandler::getAnswers, gson::toJson);
+//        Spark.delete("/answer/:answerID", answerHandler::deleteAnswer, gson::toJson);
+//
 
 
        /* PostStore postStore = new PostStoreImpl();
--- src/main/java/edu/cooper/ee/ece366/LusterCode/handler/UserHandler.java
+++ src/main/java/edu/cooper/ee/ece366/LusterCode/handler/UserHandler.java
@@ -1,48 +1,50 @@
-//package edu.cooper.ee.ece366.LusterCode.handler;
-//import edu.cooper.ee.ece366.LusterCode.service;
-//import edu.cooper.ee.ece366.LusterCode.model.User;
-//import edu.cooper.ee.ece366.LusterCode.store;
-//
-//public class UserHandler {
-//// ###################### Creating/removing/modifying USERS ###################### //
-//    private final Service service;
-//    private final Gson gson;
-//
-//    public UserHandler(Service service, Gson gson) {
-//        this.service = service;
-//        this.gson = gson;
-//    }
-//
-//    //Handler for user creation
-//    public String userMake(Request req) {
-//        String name = req.params(":field1");
-//        String pass = req.params(":field2");
-//        String firstName = req.params(":field2");
-//        String lastName = req.params(":field1");
-//        String email = req.params(":field2");
-//
-//        return service.createUser(name, pass, firstName, lastName, email)
-//    }
-//
-//    //Handler for user deletion
-//    public String userRemover(Request req) {
-//        String pass = req.params(":field2");
-//        String name = req.params(":field1");
-//
-//        return service.removeUser(name, pass);
-//    }
-//
-//    //Handler for password modification
-//    public String passChange(Request req) {
-//        String oldPass = req.params(":field2");
-//        String newPass = req.params(":field3");
-//        String name = req.params(":field1");
-//
-//        return service.changePassword(name, oldPass, newPass);
-//    }
-//
-//    //Check if user in database
-//    public boolean isUser(String name) {
-//        return service.isUser(name);
-//    }
-//}
+package edu.cooper.ee.ece366.LusterCode.handler;
+
+import edu.cooper.ee.ece366.LusterCode.model.User;
+import edu.cooper.ee.ece366.LusterCode.service.UserService;
+import spark.Request;
+import com.google.gson.Gson;
+
+public class UserHandler {
+// ###################### Creating/removing/modifying USERS ###################### //
+    private final UserService userService;
+    private final Gson gson;
+
+    public UserHandler(UserService userService, Gson gson) {
+        this.userService = userService;
+        this.gson = gson;
+    }
+
+    //Handler for user creation
+    public User userMake(Request req) {
+        String name = req.params(":field1");
+        String pass = req.params(":field2");
+        String firstName = req.params(":field2");
+        String lastName = req.params(":field1");
+        String email = req.params(":field2");
+
+        return userService.createUser(name, pass, firstName, lastName, email);
+    }
+
+    //Handler for user deletion
+    public User userRemover(Request req) {
+        String pass = req.params(":field2");
+        String name = req.params(":field1");
+
+        return userService.removeUser(name, pass);
+    }
+
+    //Handler for password modification
+    public String passChange(Request req) {
+        String oldPass = req.params(":field2");
+        String newPass = req.params(":field3");
+        String name = req.params(":field1");
+
+        return userService.changePassword(name, oldPass, newPass);
+    }
+
+    //Check if user in database
+    public boolean isUser(String name) {
+        return userService.isUser(name);
+    }
+}
