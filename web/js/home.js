561f32993ef417b547d72457ad34356333c93e24
jasonkurian <jkurian49@gmail.com>
2020-04-20 20:09:21 -0400

    view my posts

--- web/js/home.js
+++ web/js/home.js
@@ -8,7 +8,6 @@ function closeForm() {
 }
 
 async function newPost() {
-    // this successfully creates a new post but does not return it correctly
     var form = document.getElementById("newpost");
     var body = form.elements.body.value;
 
@@ -34,17 +33,83 @@ async function newPost() {
     });
 }
 
-function displayMyPosts() {
-    var posts = getMyPosts("Jason");
+function displayMyPosts(data) {
+    var noOfPosts = data.length;
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+    table.style.width = '50%';
+    table.setAttribute('border', '1');
+    table.setAttribute('cellspacing', '0');
+    table.setAttribute('cellpadding', '5');
+    table.setAttribute('height', '400px');
+
+    // retrieve column header ('Name', 'Email', and 'Mobile')
+
+    var col = []; // define an empty array
+    for (var i = 0; i < noOfPosts; i++) {
+        for (var key in data[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+
+    // CREATE TABLE HEAD .
+    var tHead = document.createElement("thead");
+
+
+    // CREATE ROW FOR TABLE HEAD .
+    var hRow = document.createElement("tr");
+
+    // ADD COLUMN HEADER TO ROW OF TABLE HEAD.
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = col[i];
+        hRow.appendChild(th);
+    }
+    tHead.appendChild(hRow);
+    table.appendChild(tHead);
+
+    // CREATE TABLE BODY .
+    var tBody = document.createElement("tbody");
+
+    // ADD COLUMN HEADER TO ROW OF TABLE HEAD.
+    for (var i = 0; i < noOfPosts; i++) {
+
+        var bRow = document.createElement("tr"); // CREATE ROW FOR EACH RECORD .
+
+
+        for (var j = 0; j < col.length; j++) {
+            var td = document.createElement("td");
+            td.innerHTML = data[i][col[j]];
+            bRow.appendChild(td);
+        }
+        tBody.appendChild(bRow)
+
+    }
+    table.appendChild(tBody);
+
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("mypoststable");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+    /*
+        for (var i=0;i<data.length;i++) {
+            var username = data[i].username;
+            var content  = data[i].content;
+        } */
 }
 
-async function getMyPosts(username) {
+async function getMyPosts() {
+    var username = "Jason";
     fetch('http://localhost:4567/posts/'+username).then(function (response) {
         // The API call was successful!
         return response.json();
     }).then(function (data) {
         // This is the JSON from our response
         console.log(data);
+        displayMyPosts(data);
     }).catch(function (err) {
         // There was an error
         console.warn('Something went wrong.', err);
